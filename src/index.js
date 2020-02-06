c1e52d3ecb3ca1d60d6f2725d7ce67a164770904
tania <tani.zhelezniak@gmail.com>
2020-02-06 15:31:20 +0200

    test

--- src/index.js
+++ src/index.js
@@ -1,9 +1,13 @@
+
+import * as $ from 'jquery';
 import Post from '@models/Post';
 import json from './assets/test';
 import './css/main';
 import imgTest from '@/assets/illustrator';
 
 const post = new Post('test webpack', imgTest);
+
+$('pre').html(post.toStr());
 
 console.log(post.toStr(imgTest));
 console.log(json);
